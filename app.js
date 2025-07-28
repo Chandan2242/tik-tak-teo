b3d90550830f7783820078169837e61866132d8f
chandan Pragapati <chandan22781310@gmail.com>
2025-07-28 21:17:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let reset = document.querySelectorAll(".reset")
+
+let count = 1;
+
+
+
+function filldata(event) {
+  let win = "";
+  if (count <= 9) {
+    if (count % 2 != 0) {
+      event.target.innerHTML = "X"
+    } else {
+      event.target.innerHTML = "O";
+    }
+    count++;
+    // console.log(event.target.innerText);
+    win = event.target.innerText ;
+
+
+    if (checkwin(event)) {
+      // console.log(event)
+      setTimeout(() => {
+        console.log(win);
+        alert("winner "+win);
+        count=0;
+        resetdata();
+
+
+      })
+
+    }
+  } else {
+    alert("game over");
+    resetdata();
+  }
+
+}
+
+
+let arr = "";
+console.log(reset)
+function resetdata() {
+  for (i = 0; i <= 8; i++) {
+    reset[i].innerHTML = "";
+    // console.log(arr);
+  }
+}
+function Ddata(div) {
+  return document.getElementsByClassName(reset).innerHTML;
+}
+
+function checkwin(event) {
+  if (checkcondition(one, two, three) || checkcondition(one, five, nine) || checkcondition(one, four, seven) || checkcondition(three, five, seven) || checkcondition(four, five, six) || checkcondition(three, six, nine) || checkcondition(seven, eight, nine) || checkcondition(two, five, eight)) {
+    // console.log(event)
+    return true;
+  }
+}
+
+
+function checkcondition(one, two, three) {
+  if (one.innerHTML != "" && two.innerHTML != "" && three.innerHTML != "" && one.innerHTML == two.innerHTML && two.innerHTML == three.innerHTML) {
+    return true;
+  }
+}
